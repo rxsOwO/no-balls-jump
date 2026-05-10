baab091685788b055ced4ea0497c81a4ab5be5aa
Famtastic3001 <famtastic3000@gmail.com>
2026-05-09 23:57:21 -0400

    some level desing

--- Scenes/jump_platform.gd
+++ Scenes/jump_platform.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@export var jumpPadHeight = -900
+
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.velocity.y = -1500
+		body.velocity.y = jumpPadHeight
 	pass # Replace with function body.
